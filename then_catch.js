59020d8cd079fc81de7375046dd15952d4cefcbe
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-01-12 10:07:31 -0300

    creating interative interface

--- then_catch.js
+++ then_catch.js
@@ -1,14 +1,38 @@
 const axiosRequest = require("axios");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 console.log("Promisses study");
+console.log("welcome!");
 
-axiosRequest.get("http://www.boredapi.com/api/activity")
+rl.question("testing api or erro?",(target) => {
+    console.log(`target is: ${target}`);
+    const userChoice = target;
+    rl.close()
+});
+
+
+if(userChoice === "api"){
+    axiosRequest.get("http://www.boredapi.com/api/activity")
     .then(response => {
         console.log("response:",response.data.activity)
     })
     .catch(error => {
         console.log("error:",error)
     });
-
+}else if(userChoice === "error"){
+    axiosRequest.get("http://httpstat.us/404")
+    .then(response => {
+        console.log("response:",response.data.activity)
+    })
+    .catch(error => {
+        console.log("error:",error)
+    });
+}else{
+    console.log("default case!")
+};
 //then or catch to see the result of it
 
