6abc670dfd1f61e7eb86b86813cfce55706ac7c8
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-01-12 11:21:36 -0300

    User basic prompt interface

--- then_catch.js
+++ then_catch.js
@@ -4,35 +4,55 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+const prompt = (query) => new Promise((resolve) => rl.question(query,resolve));
 
 console.log("Promisses study");
 console.log("welcome!");
 
-rl.question("testing api or erro?",(target) => {
-    console.log(`target is: ${target}`);
-    const userChoice = target;
-    rl.close()
-});
+(async() => {
+    try{
+        const target = await prompt("Are you testing for API or ERROR?")
+        console.log(`OK. ${target} it is!`);
+        var url = checkTarget(target);
+        console.log("Chosen URL:",url);
+        rl.close();
+        runApi(url);
+    }catch(e){
+        console.error("error while prompting",e)
+    }
+})();
 
+// rl.on("close",() => process.exit(0))
 
-if(userChoice === "api"){
-    axiosRequest.get("http://www.boredapi.com/api/activity")
-    .then(response => {
-        console.log("response:",response.data.activity)
-    })
-    .catch(error => {
-        console.log("error:",error)
-    });
-}else if(userChoice === "error"){
-    axiosRequest.get("http://httpstat.us/404")
-    .then(response => {
-        console.log("response:",response.data.activity)
-    })
-    .catch(error => {
-        console.log("error:",error)
-    });
-}else{
-    console.log("default case!")
+const checkTarget = (target) => {
+    let url = ""
+    switch(target){
+        case "API":
+        case "api":
+        case "Api":
+            url = "http://www.boredapi.com/api/activity"
+            break
+        case "Error":
+        case "ERROR":
+        case "Error":
+            url = "https://httpstat.us/404"
+        default:
+            url = "https://httpstat.us/404"
+    };
+    return url
 };
+
+const runApi = (url) => {
+    axiosRequest.get(url)
+    .then(response => {
+        console.log("response:",response.data.activity)
+    })
+    .catch(error => {
+        console.log("error:",error)
+    });
+};
+//applying then and catch
+
+
 //then or catch to see the result of it
 
